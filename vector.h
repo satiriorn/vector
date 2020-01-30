ef4e3c016debc0bad52cb2c62a584274ecc0f11e
satiriorn <satiriorn@gmail.com>
2020-01-30 13:33:11 +0200

    Inception interface v.0.1

--- vector.h
+++ vector.h
@@ -2,5 +2,19 @@
 #define VECTOR_H
 #include <Arduino.h>
 
+template<typename T>
+class vector
+{
+	private:
+		T* elem;
+		int scale;
+	public:
+		explicit vector(int s);
+		~vector(){delete[] elem;}
+		T& operator[](int i);
+		const T& operator[](int i) const;
+		int size()const{return scale}
+}
+
 
 #endif
